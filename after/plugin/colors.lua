e6eebce10ec6b550fbf44d90b18262e326daeb63
MuriMan <abrarrahim261@gmail.com>
2023-01-11 22:17:39 +0600

    Removed ColorMyPencils() function

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -1,13 +1,6 @@
-function ColorMyPencils(color, italic)
-    color = color or "gruvbox"
-    italic = italic or true
+vim.g.gruvbox_improved_warnings = true
+vim.g.gruvbox_italic = true
+vim.cmd.colorscheme("gruvbox")
 
-    vim.g.gruvbox_improved_warnings = true
-    vim.g.gruvbox_italic = italic
-    vim.cmd.colorscheme(color)
-
-    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-    vim.api.nvim_set_hl(0, "FloatBorder", {bg = "none"})
-end
-
-ColorMyPencils()
+vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+vim.api.nvim_set_hl(0, "FloatBorder", {bg = "none"})
