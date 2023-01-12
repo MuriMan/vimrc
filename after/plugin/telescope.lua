8cf48a9c69d075ebd5f05eebb2512cad3f14acdc
MuriMan <abrarrahim261@gmail.com>
2023-01-12 12:18:33 +0600

    Minor changes

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -4,7 +4,5 @@ vim.keymap.set('n', '<leader>fs', function()
 	builtin.grep_string({ search = vim.fn.input("Grep: ") })
 end)
 
-vim.keymap.set('n', '<leader>fr', require('telescope.builtin').oldfiles, { desc = '[?] Find recently opened files' })
-vim.keymap.set('n', '<leader>fb', require('telescope.builtin').buffers, { desc = '[ ] Find existing buffers' })
-
-vim.keymap.set("n", "<leader>sin", builtin.current_buffer_fuzzy_find)
+vim.keymap.set('n', '<leader>fr', builtin.oldfiles, { desc = '[?] Find recently opened files' })
+vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = '[ ] Find existing buffers' })
