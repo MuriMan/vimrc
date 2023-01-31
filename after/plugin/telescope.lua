d8643ed585f2015600f80c3cd1a9259dc2ee7282
MuriMan <abrarrahim261@gmail.com>
2023-01-31 19:41:35 +0600

    Minor changes

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,9 +1,10 @@
 local trouble = require("trouble.providers.telescope")
 
 local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-vim.keymap.set('n', '<leader>fs', function()
-	builtin.grep_string({ search = vim.fn.input("Grep: ") })
+vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
+vim.keymap.set("n", "<leader>fg", builtin.git_files, {})
+vim.keymap.set("n", "<leader>fs", function() 
+    builtin.grep_string({ search = vim.fn.input("Grep -> ")})
 end)
 
 require("telescope").setup {
@@ -11,6 +12,11 @@ require("telescope").setup {
         mappings = {
             i = { ["<c-t>"] = trouble.open_with_trouble },
             n = { ["<c-t>"] = trouble.open_with_trouble },
+        },
+        pickers = {
+            find_files = {
+                theme = "dropdown",
+            },
         },
     },
 }
