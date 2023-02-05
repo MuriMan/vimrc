2d5bfd758bb6dea7142547620bb5107f96f23cc7
MuriMan <abrarrahim261@gmail.com>
2023-02-05 20:08:55 +0600

    Added Color() function

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -1,16 +1,24 @@
-vim.g.gruvbox_improved_warnings = true
-vim.g.gruvbox_italic = true
-vim.g.everforest_enable_italics = 1
-vim.g.everforest_transparent_background = 1
-vim.g.everforest_dim_inactive_windows = 1
-vim.cmd.colorscheme("everforest")
+function Color(color)
+    color = color or "everforest"
 
-vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-vim.api.nvim_set_hl(0, "FloatBorder", {bg = "none"})
-vim.api.nvim_set_hl(0, "Pmenu", {bg = "none"})
+    vim.g.gruvbox_improved_warnings = true
+    vim.g.gruvbox_italic = true
 
-vim.api.nvim_set_hl(0, "TabLine", { bg = "none" })
-vim.api.nvim_set_hl(0, "TabLineSel", { bg = "none", fg = "#928374"})
-vim.api.nvim_set_hl(0, "TabLineFill", { bg = "none" })
-vim.api.nvim_set_hl(0, "VertSplit", { bg = "none" })
+    vim.g.everforest_enable_italics = 1
+    vim.g.everforest_transparent_background = 1
+    vim.g.everforest_dim_inactive_windows = 1
+
+    vim.cmd.colorscheme(color)
+
+    vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+    vim.api.nvim_set_hl(0, "FloatBorder", {bg = "none"})
+    vim.api.nvim_set_hl(0, "Pmenu", {bg = "none"})
+
+    vim.api.nvim_set_hl(0, "TabLine", { bg = "none" })
+    vim.api.nvim_set_hl(0, "TabLineSel", { bg = "none", fg = "#928374"})
+    vim.api.nvim_set_hl(0, "TabLineFill", { bg = "none" })
+    vim.api.nvim_set_hl(0, "VertSplit", { bg = "none" })
+end
+
+Color()
