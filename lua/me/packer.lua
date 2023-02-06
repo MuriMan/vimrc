198e3d90594d48e682174d3d5558ff9d6383ebd3
MuriMan <abrarrahim261@gmail.com>
2023-02-06 21:04:36 +0600

    Minor changes

--- lua/me/packer.lua
+++ lua/me/packer.lua
@@ -1,5 +1,6 @@
 return require('packer').startup(function(use)
     use 'wbthomason/packer.nvim'
+
     use {
         'nvim-telescope/telescope.nvim', tag = '0.1.0',
         requires = { { 'nvim-lua/plenary.nvim' } }
