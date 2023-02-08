61dcd151a594af3b66e4de16156ce1722e0e7c8d
MuriMan <abrarrahim261@gmail.com>
2023-02-08 23:10:19 +0600

    Changed TabLine colours

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -15,9 +15,9 @@ function TransColor(color)
     vim.api.nvim_set_hl(0, "FloatBorder", { bg = "none" })
     vim.api.nvim_set_hl(0, "Pmenu", { bg = "none" })
 
-    vim.api.nvim_set_hl(0, "TabLine", { bg = "none" })
-    vim.api.nvim_set_hl(0, "TabLineSel", { bg = "none", fg = "#928374"})
-    vim.api.nvim_set_hl(0, "TabLineFill", { bg = "none" })
+    vim.api.nvim_set_hl(0, "TabLine", { bg = "none", fg = "#928374"})
+    vim.api.nvim_set_hl(0, "TabLineFill", { bg = "none", fg = "#928374"})
+    vim.api.nvim_set_hl(0, "TabLineSel", { bg = "none" })
     vim.api.nvim_set_hl(0, "VertSplit", { bg = "none" })
 
     vim.api.nvim_set_hl(0, "SignColumn", { bg = "none" })
